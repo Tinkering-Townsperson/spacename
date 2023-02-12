202f2b8d34ea9d6f410a594bafbfe91ec0164422
tinkering-townsperson <h2o.Drop2010@gmail.com>
2023-02-11 19:53:10 -0800

    Update code/tests

--- src/spacename/__init__.py
+++ src/spacename/__init__.py
@@ -1,1 +1,96 @@
 __version__ = "1.0.0"
+
+class Namespace(object):
+	def __init__(self, **kwargs) -> None:
+		"""Initialize namespace object.
+
+		Args:
+			**kwargs (any, optional): Attributes to populate namespace with.
+		"""
+
+		self.fill_from_dict(kwargs)
+	
+	def fill_from_dict(self, dictionary: dict) -> None:
+		"""Populate Namespace using dict.
+
+		Args:
+			dictionary (dict): Dict to populate namespace with.
+		"""
+
+		for key in dictionary:
+			setattr(self, key, dictionary[key])
+	
+	def to_dict(self) -> dict:
+		"""Return dict of self.
+
+		Returns:
+			dict: Dict of self
+		"""
+		return self.__dict__
+
+	def __repr__(self) -> str:
+		"""Return string representation of self.
+
+		Returns:
+			str: String representation of self.
+		"""
+		
+		type_name = type(self).__name__
+		arg_strings = []
+		star_args = {}
+		for arg in self._get_args():
+			arg_strings.append(repr(arg))
+		for name, value in self._get_kwargs():
+			if name.isidentifier():
+				arg_strings.append('%s=%r' % (name, value))
+			else:
+				star_args[name] = value
+		if star_args:
+			arg_strings.append('**%s' % repr(star_args))
+		return '%s(%s)' % (type_name, ', '.join(arg_strings))
+
+	def __eq__(self, other) -> bool:
+		"""Check that self is equal to other.
+
+		Args:
+			other (Namespace): Object to check against
+
+		Returns:
+			bool: Is self equal to other?
+		"""		
+		return vars(self) == vars(other) if isinstance(other, type(self)) else NotImplemented
+
+	def __contains__(self, key)-> bool:
+		"""Check if self contains given key.
+
+		Args:
+			key (str): Key to look for
+
+		Returns:
+			bool: Is key in self?
+		"""			
+		return key in self.__dict__
+
+	def __iter__(self):
+		"""Return iterator of self.
+
+		Returns:
+			list_iterator: Iterator of self
+		"""
+		return iter(self._get_kwargs())
+
+	def _get_kwargs(self) -> list[tuple[str]]:
+		"""Return keyword arguments of self in a list of tuples.
+
+		Returns:
+			list[tuple[str]]: List of self's keyword arguments.
+		"""		
+		return list(self.__dict__.items())
+
+	def _get_args(self) -> list:
+		"""Return arguments of self in  list.
+
+		Returns:
+			list: List of self's arguments
+		"""		
+		return []
--- tests/test_spacename.py
+++ tests/test_spacename.py
@@ -1,4 +1,70 @@
-from spacename import __version__
+from spacename import __version__, Namespace
 
 def test_version():
 	assert __version__ == "1.0.0"
+
+def test_init():
+	ns = Namespace(spam="foo", eggs="bar", bacon="baz")
+
+	assert ns.spam == "foo"
+	assert ns.eggs == "bar"
+	assert ns.bacon == "baz"
+
+def test_from_dict():
+	dictionary = {
+		"spam": "foo",
+		"eggs": "bar",
+		"bacon": "baz",
+	}
+	ns = Namespace()
+	ns.fill_from_dict(dictionary)
+
+	assert ns.spam == "foo"
+	assert ns.eggs == "bar"
+	assert ns.bacon == "baz"
+
+def test_to_dict():
+	ns = Namespace(spam="foo", eggs="bar", bacon="baz")
+	dictionary = ns.to_dict()
+
+	assert dictionary == {
+		"spam": "foo",
+		"eggs": "bar",
+		"bacon": "baz",
+	}
+
+def test_repr():
+	ns = Namespace(spam="foo", eggs="bar", bacon="baz")
+	
+	assert repr(ns) == "Namespace(spam='foo', eggs='bar', bacon='baz')"
+
+def test_eq():
+	ns1 = Namespace()
+	ns2 = Namespace()
+	
+	assert ns1 == ns2
+
+def test_contains():
+	ns = Namespace(spam="foo", eggs="bar", bacon="baz")
+	
+	assert "spam" in ns
+	assert "eggs"  in ns
+	assert "bacon" in ns
+
+def test_iter():
+	ns = Namespace(spam="foo", eggs="bar", bacon="baz")
+	s = ""
+	for key, value in iter(ns):
+		s += f"{key}={value},\n"
+		
+	assert s == "spam=foo,\neggs=bar,\nbacon=baz,\n"
+
+def test_get_kwargs():
+	ns = Namespace(spam="foo", eggs="bar", bacon="baz")
+	kwargs = ns._get_kwargs()
+
+	assert kwargs == [("spam", "foo"), ("eggs", "bar"), ("bacon", "baz")]
+
+def test_get_args():
+	ns = Namespace()
+	assert not ns._get_args()
